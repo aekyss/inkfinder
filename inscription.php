79317088ee5a64a6f06b1ec807cc55392390e12a
Cinthia <cinthiago0@gmail.com>
2016-03-30 00:23:32 +0200

    ajout d'un message qui confirme l'inscription

--- inscription.php
+++ inscription.php
@@ -1,3 +1,23 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <title>Inscription</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href='https://fonts.googleapis.com/css?family=Raleway:400,300' rel='stylesheet' type='text/css'>
+    <link rel="icon" type="image/png" href="img/favicon/favicon.ico" />
+    <style type="text/css">
+        p{
+            margin-top:20%;
+            text-align:center;
+            font-size:18pt;
+            font-family:'Raleway', sans-serif;
+            color: #a40000;
+            line-height:1.8em;
+        }
+    </style>
+</head>
+    <body>
 <?php
 include_once("connexion.inc");//connexion à la BD   
 
@@ -12,9 +32,11 @@ else if(isset($_POST['homme'])){
 $req = $bd->query("INSERT INTO `utilisateur`(`pseudo`,`mdp`,`email`,`sexe`,`date`) VALUES ('".
                   $_POST['pseudo']."','".$_POST['password']."','".$_POST['email']."','".$sexe."','".$_POST['date']."')");
 
-echo "ouaip";
-
 $req->closeCursor();
-        
+
+echo "<p>Vous êtes officiellement inscrit dans la communauté Ink finder :)</p>";
+header ("Refresh: 3;URL=ink-finder.php");
 				
 ?>
+    </body>
+</html>
